39a6737cfbb0bb87e8699b319101d6a9a7eb7bf1
octavpani <unopgads@gmail.com>
2021-01-04 17:46:59 +0900

    aa

---
+++ src/main/java/com/example/demo/Utils.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.net.URI;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class Utils {
+	static public String pathWithPage(String base, Pageable pageable, Object... args) {
+		UriComponentsBuilder builder = UriComponentsBuilder.newInstance().path(base);
+		for (int i = 0; i < args.length; i += 2) {
+			if (i >= args.length - 1) break;
+			builder.queryParam((String) args[i], args[i + 1]);
+		}
+		builder.queryParam("page", pageable.getPageNumber());
+		URI url = builder.build().encode().toUri();
+		return url.toString();
+
+	}
+
+	static public String pathWithSort(String base, Pageable pageable, Object... args) {
+		UriComponentsBuilder builder = UriComponentsBuilder.newInstance().path(base);
+		for (int i = 0; i < args.length; i += 2) {
+			if (i >= args.length - 1) break;
+			builder.queryParam((String) args[i], args[i + 1]);
+		}
+		pageable.getSort().forEach(o -> {
+			builder.queryParam("sort", o.getProperty() + "," + o.getDirection());
+		});
+		URI url = builder.build().encode().toUri();
+		return url.toString();
+	}
+}
