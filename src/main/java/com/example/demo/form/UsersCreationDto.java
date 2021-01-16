76f474f8bca8b731802809239f509b841c9001d2
octavpani <unopgads@gmail.com>
2021-01-16 17:36:08 +0900

    add validator for users

--- src/main/java/com/example/demo/form/UsersCreationDto.java
+++ src/main/java/com/example/demo/form/UsersCreationDto.java
@@ -3,11 +3,15 @@ package com.example.demo.form;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.example.demo.model.SiteUser;
 
 import lombok.Data;
 @Data
+
 public class UsersCreationDto {
+	 @Valid
 	 private List<SiteUser> users = new ArrayList<SiteUser>();
 
 	 public void addSiteUser(SiteUser user) {
