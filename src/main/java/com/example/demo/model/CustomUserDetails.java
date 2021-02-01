b9156884eba7140d341e5eeb9047ae4ca4fc06d6
octavpani <unopgads@gmail.com>
2021-02-01 11:31:09 +0900

    express avatar on layout

---
+++ src/main/java/com/example/demo/model/CustomUserDetails.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+@SuppressWarnings("serial")
+public class CustomUserDetails implements UserDetails {
+	private SiteUser user;
+
+	public CustomUserDetails(SiteUser user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+		grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+		return grantedAuthorities;
+	}
+
+	public String getAvatar() {
+		return user.getAvatar();
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
--- src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -1,16 +1,11 @@
 package com.example.demo.service;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.CustomUserDetails;
 import com.example.demo.model.SiteUser;
 import com.example.demo.repository.SiteUserRepository;
 
@@ -28,9 +23,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		if(user == null) {
 			throw new UsernameNotFoundException(username + "not found");
 		}
-		return createUserDetails(user);
+		return new CustomUserDetails(user);
 	}
-	public User createUserDetails(SiteUser user) {
+	/*public User createUserDetails(SiteUser user) {
 		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
 		//SpringSecurityで、hasRoleすると、自動で"ROLE_"が付加される為必要な処理
 		grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
